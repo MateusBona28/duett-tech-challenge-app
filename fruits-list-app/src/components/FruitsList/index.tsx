e0a366afac1aa975b7e2ca99b543e4f2b700b51c
mateus <mateusbona28@gmail.com>
2023-03-16 13:46:41 -0300

    ajustes gerais de estilização nos componentes

--- fruits-list-app/src/components/FruitsList/index.tsx
+++ fruits-list-app/src/components/FruitsList/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { API } from '../../api';
+import FruitCard from '../FruitCard';
 import './styles.css'
 
 const FruitsList = () => {
@@ -18,7 +19,15 @@ const FruitsList = () => {
 
     return(
         <div className='container-fruits'>
+            <div className='container-fruits__cardsDescription'>
+                <span>Descrição</span>
+                <span>Valor A</span>
+                <span>Valor B</span>
+                <span>Ação</span>
+            </div>
+            <div className='container-fruits__underline'></div>
             <button onClick={() => { console.log(fruitsList) }}>teste</button>
+            {fruitsList?.map((fruit) => <FruitCard />)}
         </div>
     )
 }
